ac285d0aa7829bb57e7eb141cc6816795f0850ab
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-02 14:52:58 +0300

    Time: 3 ms (93.72%), Space: 12.6 MB (17.18%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,8 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        palindrome = str(x)
-        if(palindrome == palindrome[::-1]):
+        # palindrome = str(x)
+        # if(palindrome == palindrome[::-1]):
+        #     return True
+        # return False
+
+        if x < 10 and x >= 0 :
             return True
-        return False
+        if x < 0 or x%10 == 0 :
+            return False
+        half = 0
+        while x > half:
+            half = half*10 +x%10
+            x//=10
+        return x == half or x == half//10
+
         
