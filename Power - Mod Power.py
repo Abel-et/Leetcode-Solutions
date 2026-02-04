e1b3f0c3960453f3e937f8ba5ab08d3f600761b8
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-04 15:45:57 +0300

    Add power calculation with modulus support
    Implement power calculation with and without modulus

---
+++ Power - Mod Power.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+
+power = pow(a,b)
+print(power)
+
+result = pow(a,b,m)
+print(result)
