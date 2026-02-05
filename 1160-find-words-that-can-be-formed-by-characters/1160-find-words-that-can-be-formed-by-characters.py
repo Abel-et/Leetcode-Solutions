acd25ef082d28888b6571e5c07d035f52fb57121
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-05 20:21:29 +0300

    Time: 42 ms (74.91%), Space: 12.7 MB (77.74%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -6,19 +6,29 @@ class Solution(object):
         :rtype: int
         """
         length =0
+        # i changed the string into a list 
         g = list(chars)
+
+        # Traverse over all words in the the word list
         for word in words:
-            print(word)
+            
+            #  if length of single word is greater that the former word return false
             if len(word) > len(chars):
                 continue
+
+                # travers in each word letters and check if they are found into the former list
             for i in word:
                 if i not in g :
                     break
                 else:
+                    # on the question it say each character could only be used onces for each word
                     p = word.count(i)
                     k = g.count(i)
+                    # checking if the a letter is appeared morethan number of former list 
                     if p > k:
                         break
             else:
+
+                #add the vaild word length
                 length += len(word)
         return length
