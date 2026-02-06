0715369b13e258da544840168fcae57379f1dab6
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-06 11:16:51 +0300

    Time: 11 ms (83.76%), Space: 22.1 MB (50.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,11 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        collector  = {}
-
+        collector  =defaultdict(list)
         for i in strs:
-            new ="".join(sorted(i))
-            if new not in collector:
-                collector[new] = []
-                collector[new].append(i)
-            else:
-                collector[new].append(i)
-           
-        values = list(collector.values())
-        angrams = sorted(values , key=len)
-        return angrams
+            collector["".join(sorted(i))].append(i) 
+        return sorted(list(collector.values()) , key=len)
+        
 
 
 
