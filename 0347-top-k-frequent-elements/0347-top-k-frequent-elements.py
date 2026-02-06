ac0ef7e3d4fdef11d8158a69485dc981ef39d1b9
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-06 11:45:33 +0300

    Time: 51 ms (5.24%), Space: 22.8 MB (58.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash = {}
+        arr = []
         
-        count_s = Counter(nums)
-    
-        sorted_dict = dict(sorted(count_s.items(), key=lambda x: x[1], reverse=True))
-
-        ans = []
-
-        for num in sorted_dict.keys():
-            if len(ans) < k:
-                ans.append(num)
-
-        return ans
+        for i in nums:
+            if i in hash:
+                hash[i] +=1
+            else:
+                hash[i] = 1
+        
+        for i in range(k):
+            maxValue = max(hash , key= hash.get)
+            arr.append(maxValue)
+            if hash:
+                hash.pop(maxValue)
+        return arr
