cc892ac63300cbb0aaaff9803f69c3f4bf4616e7
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-11 09:23:20 +0300

    Time: 4 ms (13.62%), Space: 19.1 MB (62.12%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        nums_seen = set()
-        while n not in nums_seen:
-            nums_seen.add(n)
-            n = sum(int(i) ** 2 for i in str(n))
-            if n == 1:
-                return True
-        return False
-        
+        s = set()
+
+        # check if n is not in the set and n not equal to 1
+        while n != 1 and n not in s:
+            s.add(n)
+            n = sum((int(i) ** 2 for i in str(n)))
+        return n == 1 
