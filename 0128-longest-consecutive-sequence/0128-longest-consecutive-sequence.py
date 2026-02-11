98a57ee7a0ffc687f06594a7517af8bf80a847ed
Abel <168181290+Abel-et@users.noreply.github.com>
2026-02-11 13:18:07 +0300

    Time: 57 ms (30.28%), Space: 36.7 MB (45.61%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        unique = set(nums)
+        checked = set()
+        count = 1
+        maxC = 0
+        for i in range(len(nums)):
+            # print(nums[i]+1 , unique)
+            count = 0
+            if nums[i]-1 not in unique and nums[i] not in checked:
+                checked.add(nums[i])
+                # unique.remove(nums[i]+1)
+                curr = nums[i] 
+                while curr  in unique:
+                    curr += 1
+                    count += 1
+                maxC = max(count, maxC)
+            
+        return maxC
+
+        
