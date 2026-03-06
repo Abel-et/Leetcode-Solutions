92ade011d954d5c09843c21bc901c6bed8baefdb
Abel <168181290+Abel-et@users.noreply.github.com>
2026-03-06 16:55:15 +0300

    Add solution for Middle of the Linked List

--- leetcode/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ leetcode/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,26 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # create dummy node
-        dummy = ListNode()
-        dummy.next = head
+        fast , slow = head,head
 
-        # find the lenght of the list 
-        length = 0
-        temp = dummy
-        while temp:
-            length +=1
-            temp = temp.next
-
-        # finding the middle node
-        middle = length//2
-        i = 0
-
-        # go until prev node of the middle node
-        while i < middle:
-            dummy = dummy.next
-            print(dummy.val)
-            i+=1
-        
-        # if len is even return the second middle else return 
-        return dummy if length%2 == 0 else dummy.next
+        while  fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+         
+        return slow
