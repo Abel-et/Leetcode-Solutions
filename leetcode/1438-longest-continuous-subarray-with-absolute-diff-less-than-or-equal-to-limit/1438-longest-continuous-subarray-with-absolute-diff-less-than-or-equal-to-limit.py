92875880c1e2ba73fc8777b3689b5fa35cd30d3f
Abel <168181290+Abel-et@users.noreply.github.com>
2026-03-11 17:56:12 +0300

    Add solution for Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- leetcode/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ leetcode/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -8,22 +8,28 @@ class Solution:
 
         for i in range(len(nums)):
 
-            # putting min number in min_nums deque
-
+            # putting min number in min_nums deque whichi is the curr nums is less than in deque
             while min_nums and min_nums[-1] > nums[i]:
                 min_nums.pop()
             min_nums.append(nums[i])
 
+            # putting curr max num in max deque which is the curr nums is greather than last deque
             while max_nums and max_nums[-1] < nums[i]:
                 max_nums.pop()
             max_nums.append(nums[i])
 
-            # if the condition is invalid
+            # if the condition is invalid means the max and min deque  differences is greater than limit
+            # in another way if the max value of the window and min value of the window greater the limit val
             while max_nums[0] - min_nums[0] >limit:
+
+                # shrinking the window until , the max and min value of window less than limit val
                 if max_nums[0] == nums[left]:
                     max_nums.popleft()
                 if min_nums[0] == nums[left]:
                     min_nums.popleft()
                 left +=1
+            
+            # getting the max size of subarray 
             max_size = max(max_size, i - left + 1)
+            
         return max_size
