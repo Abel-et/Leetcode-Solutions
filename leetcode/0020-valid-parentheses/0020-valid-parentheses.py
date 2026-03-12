d75276f93a44d1b5789e8043a1b3022980aa5162
Abel <168181290+Abel-et@users.noreply.github.com>
2026-03-12 11:03:16 +0300

    Add solution for Valid Parentheses

--- leetcode/0020-valid-parentheses/0020-valid-parentheses.py
+++ leetcode/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,30 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        # the game is just puting the inclosed parenthese as a key and the opposite one as a value
         parentheses = {')':'(','}':'{',']':'['}
+
+        # put in to a stack the values which are in s
         stack = []
+
+        # if the lenght of the parentheses is odd automaticall show return false
         if len(s)%2 == 1:
             return False
         for i in s:
+            # if the comming paren is inclose
             if i in parentheses:
-                if stack and stack[-1] != parentheses[i]:
+
+                # and the top of the stack is not the oposite of it return  False
+                if stack and stack[-1] != parentheses[i] or not stack:
                     return False
-                elif  stack and stack[-1] == parentheses[i]:
-                    stack.pop()
+                
+                # or the top of the stack is valid to the given inclosed parenthese delete it 
                 else:
-                    stack.append(i)
+                    stack.pop()
+                    
+            # if the give parent is opened should add to the stack
             else:
                 stack.append(i)
+        
+        # if the stack is empty return true 
         return stack == []
