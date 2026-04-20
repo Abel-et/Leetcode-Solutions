4fdf95821a901538b946a0c293a32fb27fe7a1cf
Abel <168181290+Abel-et@users.noreply.github.com>
2026-04-20 14:12:46 +0300

    Add solution for Sum of Square Numbers

--- leetcode/0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ leetcode/0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,15 +1,20 @@
 import math 
+
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # two poitner form staring and square of c  
-        left , right = 0, int(c**0.5)
-        while left <= right:
-            total = left * left + right * right
-            
-            if total == c:
-                return True
-            elif total < c:
-                left +=1
-            else:
-                right -=1
+        # using binary search  [0 , sqrt(c)]
+        for a in range(int(math.sqrt(c)) + 1):
+            target = c - (a * a)
+            low, high = 0, int(math.sqrt(c))
+
+            while low <= high:
+                mid = low + (high - low) // 2
+                mid_sqr = mid * mid
+
+                if mid_sqr == target:
+                    return True 
+                elif mid_sqr < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return False
