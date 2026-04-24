e617add5f13b727b33f3586352ba59bb4ba644ce
Abel <168181290+Abel-et@users.noreply.github.com>
2026-04-24 19:15:05 +0300

    Add solution for Search a 2D Matrix

--- leetcode/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ leetcode/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,20 +1,26 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # convet the 2D matrix into on 1D matrix
-        mat = []
-        for matr in matrix:
-            mat.extend(matr)
+        m = len(matrix[0])
+        
         
         # applay binary search algortihm 
-        left , right = 0 , len(mat) -1
+        def binary_search(row):
+            left , right = 0 , len(row) -1
 
-        while left <= right:
-            mid = (left + right)//2
+            while left <= right:
+                mid = (left + right)//2
 
-            if mat[mid] == target:
-                return True
-            elif mat[mid] > target:
-                right = mid -1
-            else:
-                left = mid +1
+                if row[mid] == target:
+                    return True
+                elif row[mid] > target:
+                    right = mid -1
+                else:
+                    left = mid +1
+            return False
+        
+        for row in matrix:
+            if row[m-1] >= target and row[0] <= target:
+                return binary_search(row)
         return False
+
+        # in place less time O(mlogn )
