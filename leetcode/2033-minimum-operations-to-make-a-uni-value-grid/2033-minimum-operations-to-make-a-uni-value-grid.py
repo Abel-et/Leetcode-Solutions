6b1303903b496f5a811bfe2626e7fcbfd2ff5dfa
Abel <168181290+Abel-et@users.noreply.github.com>
2026-04-28 21:51:45 +0300

    Add solution for Minimum Operations to Make a Uni-Value Grid

---
+++ leetcode/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        nums = []
+
+        for row in grid:
+            for val in row:
+                nums.append(val)
+
+        # check possibility
+        rem = nums[0] % x
+
+        for num in nums:
+            if num % x != rem:
+                return -1
+
+        nums.sort()
+
+        median = nums[len(nums) // 2]
+
+        ops = 0
+
+        for num in nums:
+            ops += abs(num - median) // x
+
+        return ops
